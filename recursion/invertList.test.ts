2129441a2283fa46fec951c62d6a99245b0b1c9b
Filipe Merker <filipesmerker@gmail.com>
2021-11-27 17:51:47 +0100

    Make list inversion immutable

--- recursion/invertList.test.ts
+++ recursion/invertList.test.ts
@@ -11,14 +11,15 @@ const invertList = (items: number[]): number[] => {
     return items
   }
 
-  const left = items.shift()
-  const right = items.pop()
+  const [left] = items.slice(0, 1)
+  const [right] = items.slice(-1)
+  const center = items.slice(1, -1)
 
-  return [right as number, ...invertList(items), left as number]
+  return [right as number, ...invertList(center), left as number]
 }
 
-describe('Factorial', () => {
-  it('should sequentelly multiply all numbers up to the threshold', () => {
+describe('Invert List', () => {
+  it('should invert a list of numbers', () => {
     expect(invertList([1, 2, 3, 4, 5, 6, 7, 8, 9])).toEqual([
       9, 8, 7, 6, 5, 4, 3, 2, 1
     ])
